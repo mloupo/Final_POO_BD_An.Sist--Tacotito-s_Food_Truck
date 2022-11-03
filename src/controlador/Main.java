c6aa7349434ce5e5d880114a2381859950bb81f1
Martin Sergio Lopez <martin.sergio.lopez@gmail.com>
2022-11-03 15:01:36 -0300

    elimina codigo comentado viejo

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -5,16 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //ControladorPrincipal.mostrar();
         ControladorLogin.mostrar();
-        //DB db = new DB();
-        /*Producto p = new Producto();
-        p.setNombre("Razer I version Marti DB");
-        p.setPrecio(4000);
-        p.setCodigo(1);
-        //db.agregarProducto(p);
-        //db.eliminarProducto(2);
-        db.modificarProducto(p);
-         */
-        //System.out.print(DB.obtenerProductos());
+      
     }
 
     
